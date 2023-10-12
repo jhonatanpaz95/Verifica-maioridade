6ea1aabec801221a0bf42b7f6fca9428b274ce32
Jhonatan Paz <jhonatanpaaz@gmail.com>
2023-10-12 10:52:50 -0300

    Update verifica-idade.py

--- verifica-idade.py
+++ verifica-idade.py
@@ -5,11 +5,12 @@ idade_usuario = int(input('Digite sua idade: '))  # trabalhar o input com try/ex
 maioridade = 18
 
 #mudar essa pergunta somente se o usuário for menor de 18 anos
-maior_responsavel = input('você está acompanhado por uma pessoa com idade acima de 18 anos? (sim/não): ')
-if maior_responsavel == 'sim':
-    maior_responsavel = True
-elif maior_responsavel == 'não':
-    maior_responsavel = False
+if idade_usuario < maioridade:
+    maior_responsavel = input('você está acompanhado por uma pessoa com idade acima de 18 anos? (sim/não): ')
+    if maior_responsavel == 'sim':
+        maior_responsavel = True
+    elif maior_responsavel == 'não':
+        maior_responsavel = False
 
 if idade_usuario >= maioridade:
     print('você é maior de idade, pode prosseguir...')
